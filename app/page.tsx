20c00b0a95fbceb2350cb9375baf06ca63f9b4f1
codeapp9988 <166782490+codeapp9988@users.noreply.github.com>
2024-05-06 08:39:31 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Search from "./search";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center gap-4 h-screen w-full max-w-lg px-4">
-      <h1 className="text-4xl">$USA - United States of America -  </h1><img src="https://cdn.pixabay.com/animation/2022/09/16/16/43/16-43-33-805_512.gif" alt="USA" width="16" height="16">
+      <h1 className="text-4xl">$USA - United States of America -  </h1>
+      <img src="https://cdn.pixabay.com/animation/2022/09/16/16/43/16-43-33-805_512.gif" alt="USA" width="16" height="16">
       <span>just a explorer</span>
 
       <Search />
